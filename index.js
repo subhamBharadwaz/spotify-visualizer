d1bd040bd226c31b1b8db107b9f6d84444324e5f
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-09-05 22:35:05 +0530

    :bug: fix(callback error)

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const path = require("path");
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
-const REDIRECT_URI = process.env.REDIRECT_URI;
 const FRONTEND_URI = process.env.FRONTEND_URI;
 const PORT = process.env.PORT || 8888;
 
@@ -94,7 +93,7 @@ app.get("/callback", (req, res) => {
           expires_in,
         });
 
-        res.redirect(`${FRONTEND_URI}/?${queryParams}`);
+        res.redirect(`${FRONTEND_URI}?${queryParams}`);
       } else {
         res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
       }
